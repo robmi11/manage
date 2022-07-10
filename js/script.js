bfe9909acf82da15bcdcdd423a29b4cd0a9d34ec
robmi11 <robert.mizianty@gmail.com>
2022-07-10 21:56:42 +0200

    Frontend Mentor Project complited.

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById("menu-btn");
+const nav = document.getElementById("menu");
+
+btn.addEventListener("click", () => {
+  toggleShowHideMenu();
+});
+
+const toggleShowHideMenu = () => {
+  btn.classList.toggle("open");
+  nav.classList.toggle("hidden");
+  nav.classList.toggle("flex");
+};
